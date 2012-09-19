0356d5cc64a17a7509cf9d2abcf5500385a4592c
Goog Cheng <googcheng@gmail.com>
2012-09-19 23:10:25 +0800

    do

--- PageRank/pagerank.py
+++ PageRank/pagerank.py
@@ -12,12 +12,14 @@ def pageRank(G, s = .85, maxerr = .001):
 
     # transform G into markov matrix M
     M = csc_matrix(G,dtype=np.float)
+    # sum(1):sum of each row
     rsums = np.array(M.sum(1))[:,0]
-    ri, ci = M.nonzero()
+    # rsums : [ 1.  2.  3.  2.  1.  2.  3.]
+    ri, ci = M.nonzero()   #Return the indices of non-zero
     M.data /= rsums[ri]
-
-    # bool array of sink states
+    # bool array of sink states, rsums==0
     sink = rsums==0
+    
 
     # Compute pagerank r until we converge
     ro, r = np.zeros(n), np.ones(n)
@@ -26,12 +28,12 @@ def pageRank(G, s = .85, maxerr = .001):
         # calculate each pagerank at a time
         for i in xrange(0,n):
             # inlinks of state i
-            Ii = np.array(M[:,i].todense())[:,0]
+            Ii = np.array(M[:,i].todense())[:,0]  # select i column
+            print Ii
             # account for sink states
             Si = sink / float(n)
             # account for teleportation to state i
             Ti = np.ones(n) / float(n)
-
             r[i] = ro.dot( Ii*s + Si*s + Ti*(1-s) )
 
     # return normalized pagerank
@@ -41,7 +43,6 @@ def pageRank(G, s = .85, maxerr = .001):
 
 
 if __name__=='__main__':
-    # Example extracted from 'Introduction to Information Retrieval'
     G = np.array([[0,0,1,0,0,0,0],
                   [0,1,1,0,0,0,0],
                   [1,0,1,1,0,0,0],
