03fda2e9e916fe93980fc320d0461002b113889e
jay <googcheng@gmail.com>
2016-10-29 15:36:05 +0800

    add inotify

---
+++ inotify/inotify_many_files.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/inotify.h>
+#include <limits.h>
+#include <string.h>
+
+static void             /* Display information from inotify_event structure */
+displayInotifyEvent(struct inotify_event *i)
+{
+    printf("    wd =%2d; ", i->wd);
+    //if(i->cookie > 0)
+    //    printf("cookie =%4d; ", i->cookie);
+
+    printf("mask = ");
+    if (i->mask & IN_ACCESS)        printf("IN_ACCESS ");
+    if (i->mask & IN_ATTRIB)        printf("IN_ATTRIB ");
+    if (i->mask & IN_CLOSE_NOWRITE) printf("IN_CLOSE_NOWRITE ");
+    if (i->mask & IN_CLOSE_WRITE)   printf("IN_CLOSE_WRITE ");
+    if (i->mask & IN_CREATE)        printf("IN_CREATE ");
+    if (i->mask & IN_DELETE)        printf("IN_DELETE ");
+    if (i->mask & IN_DELETE_SELF)   printf("IN_DELETE_SELF ");
+    if (i->mask & IN_IGNORED)       printf("IN_IGNORED ");
+    if (i->mask & IN_ISDIR)         printf("IN_ISDIR ");
+    if (i->mask & IN_MODIFY)        printf("IN_MODIFY ");
+    if (i->mask & IN_MOVE_SELF)     printf("IN_MOVE_SELF ");
+    if (i->mask & IN_MOVED_FROM)    printf("IN_MOVED_FROM ");
+    if (i->mask & IN_MOVED_TO)      printf("IN_MOVED_TO ");
+    if (i->mask & IN_OPEN)          printf("IN_OPEN ");
+    if (i->mask & IN_Q_OVERFLOW)    printf("IN_Q_OVERFLOW ");
+    if (i->mask & IN_UNMOUNT)       printf("IN_UNMOUNT ");
+    printf("\n");
+
+    if (i->len > 0)
+        printf("        name = %s\n", i->name);
+}
+
+#define BUF_LEN (10 * (sizeof(struct inotify_event) + NAME_MAX + 1))
+
+int
+main(int argc, char *argv[])
+{
+    int inotifyFd, wd, j;
+    char buf[BUF_LEN] __attribute__ ((aligned(8)));
+    ssize_t numRead;
+    char *p;
+    struct inotify_event *event;
+
+    #if 0
+    if (argc < 2 || strcmp(argv[1], "--help") == 0)
+        printf("%s pathname...\n", argv[0]);
+    #endif
+
+    inotifyFd = inotify_init();
+    if(inotifyFd == -1)
+    {
+        perror("inotify_init");
+        exit(EXIT_FAILURE);
+    }
+
+    //read file line by line
+    FILE *stream;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read_cnt;
+
+    stream = fopen("./file_dirs.conf", "r");
+    if(stream == NULL)
+    {
+        perror("fopen");
+        exit(EXIT_FAILURE);
+    }
+
+    while ((read_cnt = getline(&line, &len, stream)) != -1)
+    {
+        //printf("Retrieved line of length %zu :\n", read_cnt);
+        printf("%s, len %lu\n", line, strlen(line));
+        int newline_index = strlen(line) - 1;
+        *(line + newline_index) = 0;
+
+        wd = inotify_add_watch(inotifyFd, line, IN_ALL_EVENTS);
+        if(wd == -1)
+        {
+            perror("inotify_add_watch");
+            exit(EXIT_FAILURE);
+        }
+
+
+    }
+
+    free(line);
+    fclose(stream);
+
+
+    for(;;)
+    {                                  /* Read events forever */
+        numRead = read(inotifyFd, buf, BUF_LEN);
+        if (numRead == 0)
+            printf("read() from inotify fd returned 0!");
+
+        if (numRead == -1)
+        {
+            printf("read error");
+            exit(0);
+        }
+
+
+        printf("Read %ld bytes from inotify fd\n", (long) numRead);
+
+        /* Process all of the events in buffer returned by read() */
+
+        for (p = buf; p < buf + numRead;)
+        {
+            event = (struct inotify_event *) p;
+            displayInotifyEvent(event);
+
+            p += sizeof(struct inotify_event) + event->len;
+        }
+
+    }
+
+    exit(EXIT_SUCCESS);
+}
